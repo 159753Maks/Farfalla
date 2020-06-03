f974d876596a2682db8333f97d2b98f6c4386fb3
159753Maks <berkinmaks1978@gmail.com>
2020-06-03 09:02:50 +0300

    Форма
    Перенес форму в основной проект

--- js/click.js
+++ js/click.js
@@ -1,4 +1,4 @@
-document.querySelector('.page-wrapper2 button').onclick=function() {
+document.querySelector('.button__header button').onclick=function() {
     document.querySelector('.page-wrapper').classList.add('active');
 };
 document.querySelector('.right2 button').onclick=function() {

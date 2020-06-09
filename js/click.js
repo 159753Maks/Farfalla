5a7ba91dc64be2376b4c45daccd6ce3963d6ca8c
159753Maks <berkinmaks1978@gmail.com>
2020-06-09 12:54:59 +0300

    contacts full

--- js/click.js
+++ js/click.js
@@ -1,12 +1,8 @@
-document.querySelector('.button__header button').onclick = function () {
+document.querySelector('.button__block button').onclick = function () {
     document.querySelector('.page-wrapper').classList.add('active');
     document.querySelector('body').classList.add('no-overflow');
 };
 document.querySelector('.right2 button').onclick = function () {
     document.querySelector('.page-wrapper').classList.remove('active');
     document.querySelector('body').classList.remove('no-overflow');
-};
-document.querySelector('.block2__button button').onclick = function () {
-    document.querySelector('.page-wrapper').classList.add('active');
-    document.querySelector('body').classList.add('no-overflow');
 };

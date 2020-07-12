1393c9ad09fac3c043ff16ca1d387e7fc714d1d9
Paul Borodin <p.e.borodin@gmail.com>
2020-07-12 15:18:49 +0300

    Added adaptations

--- js/click.js
+++ js/click.js
@@ -1,6 +1,7 @@
 document.querySelector('.button__block button').onclick = function () {
     document.querySelector('.page-wrapper').classList.add('active');
     document.querySelector('body').classList.add('no-overflow');
+    scroll(0, 0);
 };
 document.querySelector('.right2 button').onclick = function () {
     document.querySelector('.page-wrapper').classList.remove('active');
